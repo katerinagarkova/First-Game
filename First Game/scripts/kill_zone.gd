d63681a6d026ef431df69be38c6cf013242b5ff1
Katerina Garkova <katerina.garkova@gmail.com>
2024-09-04 12:37:19 -0400

    Added filed to github

---
+++ First Game/scripts/kill_zone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer = $Timer
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+
+func _on_body_entered(body):
+	print("You died!")
+	Engine.time_scale = 0.5
+	audio_stream_player_2d.play()
+	body.get_node("CollisionShape2D2").queue_free()
+	timer.start()
+
+func _on_timer_timeout():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
